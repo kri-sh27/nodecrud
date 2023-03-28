c7fc8d5376d6c465cb9475a222e974d39b5c672c
kri-sh27 <hogalekrishna2000@gmail.com>
2023-03-28 21:58:19 +0530

    all done

--- server.js
+++ server.js
@@ -1,15 +1,97 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/productModel')
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 
 app.get("/",(req,res)=>{
         res.send("<h1>hello node API</h1>");
 });
 
 app.get("/blog",(req,res)=>{
-    res.send("<h1>Hello blog</h1>");
+    res.send("<h1>Hello blog my name is krishna</h1>");
 });
 
-app.listen(3000, ()=>{
-    console.log("running on 3000");
+
+//get all product
+app.get('/products',async(req,res)=>{
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    }catch(error){
+        res.status(500).json({message:error.message})
+
+    }
 });
+
+//update product by id
+app.put('/products/:id',async(req,res)=>{
+    try{
+            const {id}=req.params;
+            const product = await Product.findByIdAndUpdate(id,req.body);
+            console.log(product);
+            //we cannot find any product in database
+            if(!product){
+                return res.status(404).json({message: `cannot find amy product with ID ${id}`})
+            }
+            const updatedProduct = await Product.findById(id);
+            res.status(200).json(product);
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+
+})
+
+//get product byid
+app.get('/products/:id',async(req,res)=>{
+    try{
+        const {id}=req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    }
+    catch(error){
+        res.status(500).json({message:error.message})
+    }
+})
+
+//delete a product
+app.delete('/products/:id',async(req,res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+          return  res.status(404).json({message:`cannit find any product with id ${id}`})
+        }
+        res.status(200).json({product})
+    }catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+// post product by id
+app.post('/product',async (req,res)=>{
+    // console.log(req.body);
+    // res.send(req.body);
+    try{
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    }catch(error){
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.set("strictQuery",false)
+mongoose.connect('mongodb+srv://hogalekrishna27:hogalekrishna123@devtaminapi.ausdptv.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    console.log("connected to db")
+    app.listen(3000, ()=>{
+        console.log("running on 3000");
+    });
+    
+}).catch((error)=>{
+    console.log(error)
+})
